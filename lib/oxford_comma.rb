24bd69c00b46fa71b79361f3e9acf8c5790322f4
andy reyes <11710reyes@gmail.com>
2018-12-29 03:59:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 2 
     array.join(' and ')
   elsif array.length > 2
-      array.prepend('and ')
+      array[-1].prepend('and ')
   array.join(', ')
   end
 end
